7bf393f54ac9713bb6af9d32e04edd252baf0f22
Håkon Hukkelås <hakon.hukkelas@ntnu.no>
2022-03-14 15:43:53 +0100

    Update __init__.py

--- tops/logger/__init__.py
+++ tops/logger/__init__.py
@@ -1,3 +1,3 @@
 from .logger import (
     log, add_dict, add_scalar, add_images,
-    epoch, global_step, step, step_epoch, finish, warn_once)
+    epoch, global_step, step, step_epoch, finish, warn_once, warn)
