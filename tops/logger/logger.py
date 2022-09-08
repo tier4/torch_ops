3bc57e2da70c7f47aa632885ddf5e807ac593639
Haakon Hukkelaas <hakon.hukkelas@ntnu.no>
2022-09-08 11:41:20 +0200

    fix: resume/loading logs for distributed training

--- tops/logger/logger.py
+++ tops/logger/logger.py
@@ -216,9 +216,10 @@ def init(
     output_dir = Path(output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
     _output_dir = output_dir
-    _resume()
+
     if rank() != 0:
         return
+    _resume()
     _write_metadata()
     _backends = []
     for backend in backends:
